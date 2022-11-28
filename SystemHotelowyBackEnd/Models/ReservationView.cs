8d7ea60877b80ecad9434f3b265eafdcf9470622
Dagmara <d.jura1019@gmail.com>
2022-11-28 11:34:58 +0100

    Logowanie, Rejestracja, Rezerwacja
    Logowanie, Rejestracja, Okno rezerwacji, Email potwierdzający rejestracje jeszcze nie działa, Rezerwacja jeszcze nie działa, tylko okno

---
+++ SystemHotelowyBackEnd/Models/ReservationView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SystemHotelowyBackEnd.Models
+{
+    public class ReservationView
+    {
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; }
+        [StringLength(100)]
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public int Country { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public int RoomType { get; set; }
+        public int RoomNo { get; set; }
+        public int Adults { get; set; }
+        public int Children { get; set; }
+        [Required]
+        public string CheckIn => CheckInDate.ToString("dd/MM/yyyy");
+        [Required]
+        public string CheckOut => CheckOutDate.ToString("dd/MM/yyyy");
+    }
+}
