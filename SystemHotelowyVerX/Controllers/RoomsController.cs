79c0e1ccaf58b865a2196d110993e52d6669d67c
Dagmara <d.jura1019@gmail.com>
2022-12-03 02:45:19 +0100

    VerX

---
+++ SystemHotelowyVerX/Controllers/RoomsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SystemHotelowyVer3.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace SystemHotelowyVer3.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Rooms
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Rooms.ToListAsync());
+        }
+
+        // GET: Rooms/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = await db.Rooms.FindAsync(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            return View(room);
+        }
+
+        // GET: Rooms/Create
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create()
+        {
+            return View(new Room());
+        }
+
+        // POST: Rooms/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Create([Bind(Include = "Name,Location,RoomType")] Room room)
+        {
+            if (room != null)
+            {
+                room.Isactive = true;
+                room.Date = DateTime.Now;
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Rooms.Add(room);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(room);
+        }
+
+        // GET: Rooms/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = await db.Rooms.FindAsync(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            return View(room);
+        }
+
+        // POST: Rooms/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Location,RoomType,Isactive,Date")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(room).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(room);
+        }
+
+        // GET: Rooms/Delete/5
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = await db.Rooms.FindAsync(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            return View(room);
+        }
+
+        // POST: Rooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Room room = await db.Rooms.FindAsync(id);
+            db.Rooms.Remove(room);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SystemHotelowyVerX/Models/Room.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SystemHotelowyVer3.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Location { get; set; }
+        [Required]
+        public string RoomType { get; set; }
+        public bool Isactive { get; set; }
+        public DateTime Date { get; set; }
+
+        [NotMapped]
+        public virtual string[] RoomTypes => new string[] { "Apartament", "Podwójny pokój", "Pojedyńczy pokój" };
+        //public virtual int[] RoomTypes => new int[] { 7, 8, 9 };
+
+        //[NotMapped]
+        //public virtual List<Room> Rooms => new ApplicationDbContext().Rooms.ToList();
+        //public virtual List<Booking> Bookings => 
+        // new ApplicationDbContext().Booking.ToList().Where(x => x.RoomId == Id).ToList();
+
+    }
+}
